f947b949f7021e6da510dc84d8086e14aa101aec
Olesj-Bilous <olesjvbilous@gmail.com>
2023-09-18 12:13:41 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Context, createContext, useContext } from "react";
+
+export default function defineContext<C>(
+  descriptor: string,
+  valueDescriptor?: string
+): [
+    Context: Context<null | C>,
+    hook: () => NonNullable<C>
+  ] {
+  const Context = createContext<null | C>(null)
+  const useDefinedContext = () => {
+    const value = useContext(Context)
+    if (!value)
+      throw new Error(`No${valueDescriptor ? ` ${valueDescriptor}` : ''} value was provided to ${descriptor}Context`)
+    return value
+  }
+  return [
+    Context,
+    useDefinedContext
+  ]
+}

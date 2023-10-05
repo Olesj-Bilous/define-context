a9c145a6e5ce9940c9fcd495201dd3dc47f69ce4
Olesj-Bilous <olesjvbilous@gmail.com>
2023-10-05 14:05:45 +0200

    document

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { Context, createContext, useContext } from "react";
 
+/**
+ * @description Creates a React {@link Context} with `defaultValue` set to `null`,
+ * and a hook into the context that throws a descriptive error
+ * if `value` passed to {@link Context.Provider} is `null | undefined`.
+ */
 export default function defineContext<C>(
   descriptor: string,
   valueDescriptor?: string
 ): [
     Context: Context<null | C>,
-    hook: () => NonNullable<C>
+    useContext: () => NonNullable<C>
   ] {
   const Context = createContext<null | C>(null)
   const useDefinedContext = () => {

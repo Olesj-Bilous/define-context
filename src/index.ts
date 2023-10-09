a7d93b2458e4391616b23a6949c84e7130510b84
Olesj-Bilous <olesjvbilous@gmail.com>
2023-10-09 03:12:44 +0200

    add reduction

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Context, createContext, useContext } from "react";
+import { Context, createContext, useContext, useReducer } from "react";
 
 /**
- * @description Creates a React {@link Context} with `defaultValue` set to `null`,
+ * Creates a `React.Context` with `defaultValue` set to `null`,
  * and a hook into the context that throws a descriptive error
- * if `value` passed to {@link Context.Provider} is `null | undefined`.
+ * if `value` passed to `Context.Provider` is `null | undefined`.
  */
 export default function defineContext<C>(
   descriptor: string,
@@ -23,4 +23,36 @@ export default function defineContext<C>(
     Context,
     useDefinedContext
   ]
+}
+
+/**
+ * A parametrised quantum of mutation that returns the mutated state
+ */
+export type Reduction<T> = (prevState: T, ...args: unknown[]) => T
+
+type ActionKey<T, R extends Record<string, Reduction<T>>, K extends keyof R> = {
+  action: K
+  args: Parameters<R[K]>
+}
+
+/**
+ * Prepares a wrapper around `React.useReducer`
+ * that will return a dispatcher that keys into `reductions`
+ * to prepare dispatch of parametrised actions.
+ */
+export function defineReduction<T, R extends Record<string, Reduction<T>>>(reductions: R) {
+  const reducer = <K extends keyof R>(prevState: T, { action, args }: ActionKey<T, R, K>) => reductions[action](prevState, ...args)
+  return {
+    useReduction: (initState: T) => {
+      const [state, dispatch] = useReducer(reducer, initState)
+      function dispatcher<K extends keyof R>(action: K) {
+        return {
+          dispatch(...args: Parameters<R[K]>) {
+            return dispatch({ action, args })
+          }
+        }
+      }
+      return [state, dispatcher] as [state: T, dispatcher: typeof dispatcher]
+    }
+  }
 }
